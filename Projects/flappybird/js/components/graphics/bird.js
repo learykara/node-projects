bbc81154f85fa2a79d7060159c6070c602467367
Kara Leary <learykara@gmail.com>
2015-08-03 22:55:07 -0400

    Fix file structure

--- Projects/flappybird/js/components/graphics/bird.js
+++
@@ -1,9 +0,0 @@
-var BirdGraphicsComponent = function(entity) {
-    this.entity = entity;
-};
-
-BirdGraphicsComponent.prototype.draw = function() {
-    console.log('Drawing a bird');
-};
-
-exports.BirdGraphicsComponent = BirdGraphicsComponent;
--- Projects/flappybird/js/main.js
+++
@@ -1,6 +0,0 @@
-var flappyBird = require('./flappybird');
-
-document.addEventListener('DOMContentLoaded', function() {
-    var app = new flappybird.FlappyBird();
-    app.run();
-});
