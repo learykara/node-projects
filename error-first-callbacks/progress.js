ab5742c5cb521e808c68701ce5ee165630815d5f
Kara Leary <learykara@gmail.com>
2015-08-07 23:36:56 -0400

    Refactor progress bar to use EventEmitter class

--- error-first-callbacks/progress.js
+++ error-first-callbacks/progress.js
@@ -1,68 +1,40 @@
-var ProgressBar = function() {
-  this.onStartCallback = null;
-  this.onProgressCallback = null;
-  this.onEndCallback = null;
-};
+var events = require('events');
+
+// Create new event emitter
+var progress = new events.EventEmitter();
 
 
-// Register callbacks
-ProgressBar.prototype.onStart = function(callback) {
-  console.log('Registered start callback');
-  this.onStartCallback = callback;
-}
-
-ProgressBar.prototype.onProgress = function(callback) {
-  console.log('Registered progress callback');
-  this.onProgressCallback = callback;
-}
-
-ProgressBar.prototype.onEnd = function(callback) {
-  console.log('Registered end callback');
-  this.onEndCallback = callback;
-}
-
-ProgressBar.prototype.start = function() {
-  if (!this.onStartCallback) {
-    return;
-  }
-  this.onStartCallback();
-}
-
-ProgressBar.prototype.progress = function(n) {
-  if (!this.onProgressCallback) {
-    return;
-  }
-  this.onProgressCallback(n);
-}
-
-ProgressBar.prototype.end = function() {
-  if (!this.onEndCallback) {
-    return;
-  }
-  this.onEndCallback();
-}
-
-
-// Initialize progress bar
-var progress = new ProgressBar();
-
-progress.onStart(function() {
-  for (var i=0; i<100; i++) {
-    if (i%10 === 0) {
-      progress.progress(i);
-    }
-  }
-  progress.end();
+progress.on('start', function() {
+  console.log('`start` event emitted.');
 });
 
-progress.onProgress(function(n) {
+progress.once('progress', function() {
+  console.log('`progress` event emitted for the first time.')
+})
+
+progress.on('end', function() {
+  console.log('`end` event emitted.');
+});
+
+
+progress.on('start', function() {
+  for (var i=0; i<100; i++) {
+    if (i % 10 === 0) {
+      progress.emit('progress', i);
+    }
+  }
+
+  progress.emit('end');
+});
+
+progress.on('progress', function(n) {
   console.log('Progress is at ' + n + '%');
 });
 
-progress.onEnd(function() {
-  console.log('Process completed.')
+progress.on('end', function() {
+  console.log('Process completed.');
 });
 
 
-// Start coutner
-progress.start();
+// Start counter
+progress.emit('start');
