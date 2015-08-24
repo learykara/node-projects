9432ddca809956de05ab3e38da7d3bf88e8fcc3c
Kara Leary <learykara@gmail.com>
2015-08-23 22:01:56 -0400

    Milestone 4: Add travel endpoint

--- express-routing/server.js
+++ express-routing/server.js
@@ -1,8 +1,16 @@
 var express = require('express');
-var app = express();
+var _ = require('lodash');
 
+// Import map
+var map = require('./map.json');
+
+var app = express();
 var port = process.env.PORT || 8080;
 
+// Define constants
+var START = 's';
+var GRUE = 'g';
+var END = 'e';
 
 // Initialize router
 var router = express.Router({
@@ -21,7 +29,6 @@ var directions = {
 
 // Routes
 router.get('/plot', function(req, res, next) {
-
   var journey = req.params[0].split('');
   var journeyLong = [];
 
@@ -30,6 +37,52 @@ router.get('/plot', function(req, res, next) {
   }
 
   res.send('You plan to travel ' + journeyLong.join(', then '));
+});
+
+router.get('/travel', function(req, res, next) {
+  var journey = req.params[0].split('');
+
+  // Determine start coordinates and map dimensions
+  var yLen = map.length;
+  var yPos = _.findIndex(map, function(row) {
+    return row.indexOf(START) > -1;
+  });
+
+  var xLen = map[0].length;
+  var xPos = map[yPos].indexOf(START);
+
+  // Make the journey
+  for (var i=0; i<journey.length; i++) {
+
+    if (journey[i] === 'n') { yPos--; }
+    else if (journey[i] === 's') { yPos++; }
+    else if (journey[i] === 'w') { xPos--; }
+    else if (journey[i] === 'e') { xPos++; }
+
+    // Check if we've traveled off the map
+    if (
+        xPos < 0 || xPos > xLen - 1 ||
+        yPos < 0 || yPos > yLen - 1) {
+      res.send('You off the grid, man. Peace out.');
+      return;
+    }
+
+    // What beasts await at our new location?
+    var newLocationValue = map[yPos][xPos];
+
+    if (newLocationValue === GRUE) {
+      res.send('You dead, man');
+      return;
+    }
+
+    else if (newLocationValue === END) {
+      res.send('CONGRATS BRO! YOU MADE IT!!');
+      return;
+    }
+  }
+
+  // Nothing to report. Keep on keepin' on.
+  res.send("Keep on keepin' on, bro.");
 });
 
 
