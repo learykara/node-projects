dd57e4e0cea99935c0eae9655f3ddb1179cc1fb8
Kara Leary <learykara@gmail.com>
2015-09-20 19:07:57 -0400

    milestone 2: add form fields

--- swig-pulitzer/index.js
+++ swig-pulitzer/index.js
@@ -1,3 +1,4 @@
+var bodyParser = require('body-parser');
 var Express = require('express');
 var swig = require('swig');
 
@@ -7,9 +8,17 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+app.use(bodyParser.urlencoded());
+
 app.get('/', function (req, res) {
-  res.render('index', {name: 'Kara'});
+  res.render('index');
 });
+
+app.post('/', function (req, res) {
+  console.log('Form (from querystring): ' + req.query.form);
+  console.log('Form fields (body): ' + req.body);
+  res.redirect(303, '/next');
+})
 
 app.listen(8080);
 console.log('Listening on 8080...');
