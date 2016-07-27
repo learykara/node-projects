d4ffa8da9914a48af2cefb8006758a220cbf4ab1
Kara Leary <learykara@gmail.com>
2016-07-26 14:21:12 -1000

    remove extra project files

--- how-to-npm/README.md
+++
@@ -1,1 +0,0 @@
-This package was made for th tutorial in the how-to-npm package, and does absolutely nothing.
--- musicians/spinal-tap.js
+++
@@ -1,8 +0,0 @@
-const { Bassist, Guitarist } = require('./musicians');
-
-const nigel = new Guitarist();
-nigel.tune();
-nigel.solo(8);
-
-const derek = new Bassist();
-derek.solo(4);
