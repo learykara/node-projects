8678be52da92fa9f01449f0695fdfdd61078ae38
Kara Leary <learykara@gmail.com>
2016-07-26 14:16:45 -1000

    re-branch off of master

---
+++ routes.js
@@ -0,0 +1,42 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const Storage = require('./storage');
+
+const router = express.Router();
+const jsonParser = bodyParser.json();
+
+const storage = new Storage();
+
+router.get('/items', (req, res) => {
+  res.json(storage.items);
+});
+
+router.post('/items', jsonParser, (req, res) => {
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+
+  const item = storage.add(req.body.name);
+  res.status(201).json(item);
+});
+
+router.delete('/items/:id', (req, res) => {
+  storage.delete(parseInt(req.params.id));
+  return res.sendStatus(204);
+});
+
+router.put('/items/:id', jsonParser, (req, res) => {
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+  try {
+    storage.upsert(req.body.name, parseInt(req.body.id));
+  } catch (err) {
+    console.error('Item ID must be an integer.');
+    res.sendStatus(400);
+  }
+  return res.sendStatus(200).json(item);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = require('./routes');
+
+const app = express();
+app.use(express.static('public'));
+app.use('/', router);
+
+app.listen(process.env.PORT || 8080);
