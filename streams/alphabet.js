23d741ea5e00a4e54cb6405131942b7d242cf07b
Kara Leary <learykara@gmail.com>
2016-09-20 14:53:01 -1000

    refactor to use classes

--- streams/alphabet.js
+++ streams/alphabet.js
@@ -1,23 +1,21 @@
-const stream = require('stream')
+const { Readable } = require('stream')
 
-function Alphabet(options) {
-  stream.Readable.call(this, options)
-  this._start = 'a'
-  this._end = 'z'
-  this._curr = this._start.charCodeAt(0)
-}
+class Alphabet extends Readable {
+  constructor(options) {
+    super(options)
+    this._start = 'a'
+    this._end = 'z'
+    this._curr = this._start.charCodeAt(0)
+  }
 
-Alphabet.prototype = Object.create(stream.Readable.prototype)
-Alphabet.prototype.constructor = Alphabet
-
-Alphabet.prototype._read = () => {
-  const letter = String.fromCharCode(this._curr)
-  const buf = new Buffer(letter, 'utf8')
-  // Error here -- what is it pushing `buf` onto?
-  this.push(buf)
-  this._cur++
-  if (letter === this._end) {
-    this.push(null)
+  _read() {
+    const letter = String.fromCharCode(this._curr)
+    const buf = new Buffer(letter, 'utf8')
+    this.push(buf)
+    this._curr++
+    if (letter === this._end) {
+      this.push(null)
+    }
   }
 }
 
