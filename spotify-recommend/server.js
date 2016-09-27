d90506e124e28e39089fdcffbcd885753d48d291
Kara Leary <learykara@gmail.com>
2016-09-27 13:32:29 -1000

    events in series assignment

--- spotify-recommend/server.js
+++ spotify-recommend/server.js
@@ -1,35 +1,54 @@
-var unirest = require('unirest');
-var express = require('express');
-var events = require('events');
+const express = require('express')
+const unirest = require('unirest')
+const { EventEmitter } = require('events')
 
-var app = express();
-app.use(express.static('public'));
+const app = express()
 
-var getFromApi = function(endpoint, args) {
-	var emitter = new events.EventEmitter();
-	unirest.get('https://api.spotify.com/v1/' + endpoint)
-				 .qs(args)
-				 .end(function(response) {
-						emitter.emit('end', response.body);
-				 });
-	return emitter;
+app.use(express.static('public'))
+
+const getFromApi = (endpoint, args) => {
+  const emitter = new EventEmitter()
+  unirest.
+    get(`https://api.spotify.com/v1/${endpoint}`).
+    qs(args).
+    end((response) => {
+      response.ok ?
+        emitter.emit('end', response.body)
+      : emitter.emit('error', response.code)
+    })
+
+    return emitter
 }
 
-app.get('/search/:name', function(req, res) {
-	var searchReq = getFromApi('search', {
-		q: req.params.name,
-		limit: 1,
-		type: 'artist'
-	});
+app.get('/search/:name', (req, res) => {
+  const searchReq = getFromApi('search', {
+    q: req.params.name,
+    limit: 1,
+    type: 'artist',
+  })
 
-	searchReq.on('end', function(item) {
-		var artist = item.artists.items[0];
-		res.json(artist);
-	});
+  searchReq.on('end', (item) => {
+    const artist = item.artists.items[0]
 
-	searchReq.on('error', function() {
-		res.sendStatus(404);
-	});
-});
+    if (!artist) {
+      return res.sendStatus(404)
+    }
 
-app.listen(8080);
+    const artistId = artist.id
+
+    getFromApi(`artists/${artistId}/related-artists`, {
+      limit: 5,
+    }).on('end', (item2) => {
+      artist.related = item2.artists
+      res.json(artist)
+    }).on('error', (code) => {
+      res.sendStatus(404)
+    })
+  })
+
+  searchReq.on('error', (code) => {
+    res.sendStatus(code)
+  })
+})
+
+app.listen(process.env.PORT || 8080)
