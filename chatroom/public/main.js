db7c017d0138c0ed03daa57680264472baea2828
Kara Leary <learykara@gmail.com>
2016-10-07 14:36:26 -1000

    enhance the chatroom assignment

--- chatroom/public/main.js
+++ chatroom/public/main.js
@@ -1,25 +1,113 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const socket = io()
-
-  const input = document.getElementById('input')
+const addMessage = (message, username) => {
   const messages = document.getElementById('messages')
-
-  const addMessage = (message) => {
-    const newMessage = document.createElement('div')
-    newMessage.innerText = message
-    messages.appendChild(newMessage)
+  let newMessage = document.createElement('div')
+  const messageContent = document.createElement('span')
+  messageContent.innerText = message
+  if (username) {
+    const messageUsername = document.createElement('strong')
+    messageUsername.innerText = `${username}: `
+    newMessage.appendChild(messageUsername)
+    newMessage.appendChild(messageContent)
+  } else {
+    newMessage.appendChild(messageContent)
   }
+  messages.appendChild(newMessage)
+}
 
-  input.addEventListener('keydown', (event) => {
+const notifyNewConnection = (username) => {
+  addMessage(`${username} joined the chat.`)
+}
+
+const notifyUserExit = (username) => {
+  addMessage(`${username} left the chat.`)
+}
+
+const listenForMessages = (socket) => {
+  const messageInput = document.getElementById('message')
+  let typingTimer;
+
+  messageInput.addEventListener('keyup', (event) => {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(() => {
+      console.log('finished typing')
+      finishedTyping(socket)
+    }, 3000)
+  })
+
+  messageInput.addEventListener('keydown', (event) => {
+    if (event.keyCode !== 13) {
+      clearTimeout(typingTimer)
+      socket.emit('typing', window.userName)
+      return
+    }
+
+    const message = messageInput.value
+    addMessage(messageInput.value, window.userName)
+    socket.emit('message', message, window.userName)
+    messageInput.value = ''
+  })
+
+  socket.on('message', addMessage)
+}
+
+const finishedTyping = (socket) => {
+  console.log('yooo')
+  socket.emit('doneTyping', window.userName)
+}
+
+const enterChatroom = () => {
+  const portal = document.getElementById('portal')
+  const chatroom = document.getElementById('chatroom')
+  portal.classList.add('hidden')
+  chatroom.classList.remove('hidden')
+}
+
+const listenForUsers = (socket) => {
+  const nameInput = document.getElementById('username')
+
+  nameInput.addEventListener('keydown', (event) => {
     if (event.keyCode !== 13) {
       return
     }
 
-    const message = input.value
-    addMessage(input.value)
-    socket.emit('message', message)
-    input.value = ''
+    let name = nameInput.value
+
+    if (!name) {
+      alert('Username required.')
+      return
+    }
+
+    socket.emit('newUser', name)
+    window.userName = name
+    enterChatroom()
   })
 
-  socket.on('message', addMessage)
+  socket.on('newUser', notifyNewConnection)
+  socket.on('userExit', notifyUserExit)
+}
+
+const updateTypers = (usernames) => {
+  const typers = document.getElementById('typers')
+  typers.innerText = (usernames && usernames.length) ?
+      usernames.join(', ')
+    : 'No one'
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const socket = io()
+
+  listenForMessages(socket)
+  listenForUsers(socket)
+
+  socket.on('updateUserCount', (count) => {
+    const userCounter = document.getElementById('user-count')
+    userCounter.innerText = `${count} ${count === 1 ? 'person' : 'people'}`
+  })
+
+  socket.on('typing', updateTypers)
+  socket.on('doneTyping', updateTypers)
+
+  window.addEventListener('beforeunload', (event) => {
+    socket.emit('userExit', window.userName)
+  })
 });
--- chatroom/server.js
+++ chatroom/server.js
@@ -8,12 +8,44 @@ app.use(express.static('public'))
 const server = http.Server(app)
 const io = socketIo(server)
 
+const allUsers = []
+const typingUsers = []
+
 io.on('connection', (socket) => {
   console.log('Client connected')
+  io.emit('updateUserCount', allUsers.length)
 
-  socket.on('message', (message) => {
+  socket.on('newUser', (username) => {
+    console.log(`New user detected: ${username}`)
+    socket.broadcast.emit('newUser', username)
+
+    allUsers.push(username)
+    io.emit('updateUserCount', allUsers.length)
+  })
+
+  socket.on('typing', (username) => {
+    typingUsers.indexOf(username) === -1 && typingUsers.push(username)
+    socket.broadcast.emit('typing', typingUsers)
+  })
+
+  socket.on('doneTyping', (username) => {
+    typingUsers.pop(username)
+    socket.broadcast.emit('doneTyping')
+  })
+
+  socket.on('message', (message, username) => {
     console.log(`Received message: ${message}`)
-    socket.broadcast.emit('message', message)
+    socket.broadcast.emit('message', message, username)
+  })
+
+  socket.on('userExit', (username) => {
+    console.log(`${username} left the chat.`)
+    allUsers.pop(username)
+    typingUsers.pop(username)
+
+    socket.broadcast.emit('userExit', username)
+
+    io.emit('updateUserCount', allUsers.length)
   })
 })
 
